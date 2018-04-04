3dba68e0baa9bac2c0b9dc735fa79a1aa9fce86e
duckheewon <fain9301@yahoo.com>
2018-04-04 09:31:25 +0900

    fix gs1 and usart

--- AP/Ap_Lib/gs1.c
+++ AP/Ap_Lib/gs1.c
@@ -3,9 +3,53 @@
 #include "gs1.h"
 
 GS1_DEF void GS1_Init(void);
+//get usrat queue data
+HW_USART_DEF void USART_GetData(void);
+HW_USART_DEF void USART_PutData(uint16_t uData);
+HW_USART_DEF void USART_InitQueue(void);
+HW_USART_DEF void USART_AddQueue(uint16_t uData);
+HW_USART_DEF void USART_ProcessQueue(void);
+
 
 
 GS1_DEF void GS1_Init(void)
 {
     
 }
+
+
+
+//get usrat queue data
+HW_USART_DEF void USART_GetData(void)
+{
+  uint16_t uData;
+  while(!USART_GetFlagStatus(USART2, USART_FLAG_RXNE));
+  uData = USART_ReceiveData(USART2);
+  return uData;
+}
+
+HW_USART_DEF void USART_PutData(uint16_t uData)
+{
+  while(!USART_GetFlagStatus(USART2, USART_FLAG_TXE));
+  USART_SendData(USART2, uData);
+}
+HW_USART_DEF void USART_InitQueue(void)
+{
+  getUartQueue.uHeadIndex = 0;
+  getUartQueue.uTailIndex = 0;
+}
+
+HW_USART_DEF void USART_AddQueue(uint16_t uData)
+{
+  getUartQueue.uHeadIndex = (getUartQueue.uHeadIndex + 1) & USART_QUEUE_BUF_MASK;
+  getUartQueue.uBuf[getUartQueue.uHeadIndex] = uData;
+}
+
+HW_USART_DEF void USART_ProcessQueue(void)
+{
+  while(getUartQueue.uHeadIndex != getUartQueue.uTailIndex){
+    getUartQueue.uTailIndex = (getUartQueue.uTailIndex + 1) & USART_QUEUE_BUF_MASK;
+    USART_PutData(getUartQueue.uBuf[getUartQueue.uTailIndex]);
+  }  
+}
+
--- AP/Ap_Lib/gs1.h
+++ AP/Ap_Lib/gs1.h
@@ -9,7 +9,27 @@
 #define GS1_DEF                  extern
 #endif
 
+/* struct data buffer */
+#define USART_QUEUE_BUF_SIZE(2*2)
+#define USART_QUEUE_BUF_MASK(USART_QUEUE_BUF_SIZE - 1)
+
+typedef struct
+{
+    uint16_t uBuf[USART_QUEUE_BUF_SIZE];
+    uint16_t uHeadIndex;
+    uint16_t uTailIndex;
+} getUartQueue;
+
+
 GS1_DEF void GS1_Init(void);
+//get usrat queue data
+HW_USART_DEF void USART_GetData(void);
+HW_USART_DEF void USART_PutData(uint16_t uData);
+HW_USART_DEF void USART_InitQueue(void);
+HW_USART_DEF void USART_AddQueue(uint16_t uData);
+HW_USART_DEF void USART_ProcessQueue(void);
+
+
 
 
 #endif
